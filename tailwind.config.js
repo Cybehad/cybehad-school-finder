2b8b60b2ace2c6b742eec850e990ecd116b7bfb4
laravelcommunityuganda <cybehad@gmail.com>
2025-06-09 14:39:25 +0300

    added tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,10 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './vendor/wireui/wireui/resources/**/*.blade.php',
-        './vendor/wireui/wireui/ts/**/*.ts',
-        './vendor/wireui/wireui/scr/View/**/*.php',
+        "./vendor/wireui/wireui/src/*.php",
+        "./vendor/wireui/wireui/ts/**/*.ts",
+        "./vendor/wireui/wireui/src/WireUi/**/*.php",
+        "./vendor/wireui/wireui/src/Components/**/*.php",
     ],
 
     theme: {
@@ -19,8 +20,8 @@ export default {
             },
         },
     },
-    // presets: [
-    //     require('./vendor/wireui/wireui/tailwind.config')
-    // ],
+    presets: [
+        require('./vendor/wireui/wireui/tailwind.config')
+    ],
     plugins: [forms],
 };
